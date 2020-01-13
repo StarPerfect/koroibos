21d98c55a912555b8c60c9e56d8651199eeee637
StarPerfect <StarPerfect@gmail.com>
2020-01-13 10:26:52 -0700

    Update Olympian model with total medal count

--- models/olympian.js
+++ models/olympian.js
@@ -5,6 +5,19 @@ const database = require('knex')(configuration);
 const all = () => database('olympians')
     .select()
 
+const totalMedals = (name) => database('olympians')
+    .where('Name', name)
+    .then((data) => {
+        let medalCount = 0;
+        data.forEach(function (event) {
+            if (event['Medal'] != 'NA') {
+                medalCount++
+            }
+        })
+        return medalCount;
+    });
+
 module.exports = {
-    all
-}
+    all,
+    totalMedals
+};
