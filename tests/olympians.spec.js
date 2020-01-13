eddc222eeee75824b4f0e8f226132b74007e142a
StarPerfect <StarPerfect@gmail.com>
2020-01-13 11:53:39 -0700

    Add finding youngest test to Olympians spec

--- tests/olympians.spec.js
+++ tests/olympians.spec.js
@@ -21,4 +21,20 @@ describe('Test Olympians path', () => {
             expect(res.body.olympians[0].total_medals_won).toBe(0);
         });
     });
+
+     describe('test finding youngest', () => {
+         it('happy path', async () => {
+             const res = await request(app)
+                 .get('/api/v1/olympians?age=youngest');
+
+             expect(res.statusCode).toBe(200);
+             expect(res.body).toHaveProperty('name');
+             expect(res.body['name']).toBe('Ana Iulia Dascl');
+             expect(res.body).toHaveProperty('team');
+             expect(res.body).toHaveProperty('age');
+             expect(res.body).toHaveProperty('sport');
+             expect(res.body).toHaveProperty('total_medals_won');
+             expect(res.body.total_medals_won).toBe(0)
+         })
+     })
 });
