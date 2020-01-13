022ebfa91f30f6407ae533f010f0fc27ae743f2d
StarPerfect <StarPerfect@gmail.com>
2020-01-13 08:37:13 -0700

    Update expectations with nested olympians object

--- tests/olympians.spec.js
+++ tests/olympians.spec.js
@@ -11,20 +11,21 @@ describe('Test Olympians path', () => {
         it('happy path', async () => {
             const res = await request(app)
                 .get('/api/v1/olympians');
+
             expect(res.statusCode).toBe(200);
-            expect(res.body.length).toBe(3485);
-            expect(res.body[0]).toHaveProperty('Name');
-            expect(res.body[0]).toHaveProperty('Sex');
-            expect(res.body[0]).toHaveProperty('Age');
-            expect(res.body[0]).toHaveProperty('Height');
-            expect(res.body[0]).toHaveProperty('Weight');
-            expect(res.body[0]).toHaveProperty('Team');
-            expect(res.body[0]).toHaveProperty('Games');
-            expect(res.body[0]).toHaveProperty('Sport');
-            expect(res.body[0]).toHaveProperty('Event');
-            expect(res.body[0]).toHaveProperty('Medal');
-            expect(res.body[0]['Name']).toBe('Andreea Aanei');
-            expect(res.body[0]['Team']).toBe('Romania');
+            expect(res.body.olympians.length).toBe(3485);
+            expect(res.body.olympians[0]).toHaveProperty('Name');
+            expect(res.body.olympians[0]).toHaveProperty('Sex');
+            expect(res.body.olympians[0]).toHaveProperty('Age');
+            expect(res.body.olympians[0]).toHaveProperty('Height');
+            expect(res.body.olympians[0]).toHaveProperty('Weight');
+            expect(res.body.olympians[0]).toHaveProperty('Team');
+            expect(res.body.olympians[0]).toHaveProperty('Games');
+            expect(res.body.olympians[0]).toHaveProperty('Sport');
+            expect(res.body.olympians[0]).toHaveProperty('Event');
+            expect(res.body.olympians[0]).toHaveProperty('Medal');
+            expect(res.body.olympians[0]['Name']).toBe('Andreea Aanei');
+            expect(res.body.olympians[0]['Team']).toBe('Romania');
         });
     });
 });
