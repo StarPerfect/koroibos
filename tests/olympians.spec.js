2da6f14230404143832f4ec2b0c3e00230a6d584
StarPerfect <StarPerfect@gmail.com>
2020-01-13 10:28:47 -0700

    Update test expectations with dot notation fields for first object

--- tests/olympians.spec.js
+++ tests/olympians.spec.js
@@ -14,18 +14,11 @@ describe('Test Olympians path', () => {
 
             expect(res.statusCode).toBe(200);
             expect(res.body.olympians.length).toBe(3485);
-            expect(res.body.olympians[0]).toHaveProperty('Name');
-            expect(res.body.olympians[0]).toHaveProperty('Sex');
-            expect(res.body.olympians[0]).toHaveProperty('Age');
-            expect(res.body.olympians[0]).toHaveProperty('Height');
-            expect(res.body.olympians[0]).toHaveProperty('Weight');
-            expect(res.body.olympians[0]).toHaveProperty('Team');
-            expect(res.body.olympians[0]).toHaveProperty('Games');
-            expect(res.body.olympians[0]).toHaveProperty('Sport');
-            expect(res.body.olympians[0]).toHaveProperty('Event');
-            expect(res.body.olympians[0]).toHaveProperty('Medal');
-            expect(res.body.olympians[0]['Name']).toBe('Andreea Aanei');
-            expect(res.body.olympians[0]['Team']).toBe('Romania');
+            expect(res.body.olympians[0].name).toBe('Andreea Aanei');
+            expect(res.body.olympians[0].age).toBe(22);
+            expect(res.body.olympians[0].team).toBe('Romania');
+            expect(res.body.olympians[0].sport).toBe('Weightlifting');
+            expect(res.body.olympians[0].total_medals_won).toBe(0);
         });
     });
 });
