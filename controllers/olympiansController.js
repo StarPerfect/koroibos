b0b902fc52ea514825095b37c0452160e11429c5
StarPerfect <StarPerfect@gmail.com>
2020-01-13 08:20:06 -0700

    Create OlympiansController with #index to get all olympians

---
+++ controllers/olympiansController.js
@@ -0,0 +1,14 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+const Olympian = require('../models/olympian');
+
+const index = (request, response) => {
+    Olympian.all()
+        .then((olympians) => response.status(200).json(olympians))
+};
+
+module.exports = {
+    index
+};
