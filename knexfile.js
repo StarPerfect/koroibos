e27aaa4cdeab2134e542ddedbdb0d7bcbf3af010
StarPerfect <StarPerfect@gmail.com>
2020-01-13 09:02:29 -0700

    Update Knexfile with production env and correct seeds directory

--- knexfile.js
+++ knexfile.js
@@ -26,12 +26,12 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL || 'postgres://sljeqmaqqufncm:3bad105ee219e476921e8c3c7c38926f68378aa59f96c3c85c102aa8379c03fc@ec2-174-129-33-19.compute-1.amazonaws.com:5432/d1ahlq33510u78',
     migrations: {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/production'
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
