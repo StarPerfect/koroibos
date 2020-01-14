75081a66bc4031fca2828012c578f6f5402e61f5
StarPerfect <StarPerfect@gmail.com>
2020-01-13 17:24:49 -0700

    Update events controller

--- routes/events.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const statsController = require('../../../controllers/eventsController');
-
-router.get('/', eventsController.index);
-
-module.exports = router;
