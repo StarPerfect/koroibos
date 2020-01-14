ffc2e769d5f9e592cb3ad8b6b4fa627624def354
StarPerfect <StarPerfect@gmail.com>
2020-01-14 11:18:12 -0700

    Add Medalists path to Events Router

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const eventsController = require('../../../controllers/eventsController');
 
 router.get('/', eventsController.index);
+router.get('/:id/medalists', eventsController.show);
 
 module.exports = router;
